b8b1b968537f4a7473c4cb41275c49771bb95695
fuujiro <fuujiro1997@gmail.com>
2018-09-04 09:11:03 +0800

    180904

---
+++ 180904/single-number.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    /**
+     * @param A: Array of integers.
+     * return: The single number.
+     */
+    int singleNumber(vector<int> &A) {
+        int single = 0;
+        for (const auto& i : A) {
+            single ^= i;
+        }
+
+        return single;
+    }
+};

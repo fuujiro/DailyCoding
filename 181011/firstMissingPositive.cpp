6d4641a4c9f8719f55600305aed83ca2fe697398
fuujiro <fuujiro1997@gmail.com>
2018-10-15 23:49:37 +0800

    Day 26

--- 181011/firstMissingPositive.cpp
+++ 181011/firstMissingPositive.cpp
@@ -1,22 +1,18 @@
 class Solution {
 public:
-    int firstMissingPositive (vector<int>& nums) {
-        vector<int>::iterator it;
-        int rel=1;
-        int i=1;
-        for(i=1;i<=nums.size();i++)
-        {
-            it = find(nums.begin(),nums.end(),i);
-            if(it==nums.end())
-            {
-                rel=i;
-                break;
-            }
-            else if(i==nums.size())
-            {
-              rel=i+1;  
-            }
-        }
-        return rel;
-    }
+    int firstMissingPositive (vector<int>& nums) {
+        vector<int>::iterator it;
+        int rel = 1;
+        int i = 1;
+        for(i = 1; i <= nums.size(); ++i) {
+            it = find(nums.begin(), nums.end(), i);
+            if(it == nums.end()) {
+                rel = i;
+                break;
+            } else if(i == nums.size()) {
+                rel = i + 1;
+            }
+        }
+        return rel;
+    }
 };

2118cc0c963cb4b25cee0630cb65a3a23a21a123
fuujiro <fuujiro1997@gmail.com>
2018-10-29 23:41:54 +0800

    Day 44 - num66

---
+++ 181029/plusOne.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    vector<int> plusOne(vector<int> &digits) {
+        int n = digits.size();
+        for (int i = n - 1; i >= 0; --i) {
+            if (digits[i] == 9) digits[i] = 0;
+            else {
+                digits[i] += 1;
+                return digits;
+            }
+        }
+        if (digits.front() == 0) digits.insert(digits.begin(), 1);
+        return digits;
+    }
+};

a51ef905e5cb0f1d6a83b3e0e275d04e1a218582
fuujiro <fuujiro1997@gmail.com>
2018-11-15 23:16:30 +0800

    OnlinePlatform v1.0

---
+++ 181107/sortColor.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    void sortColors(vector<int>& nums) {
+	int n2 = -1, n1 = -1, n0 = -1,n=nums.size();
+	for (int i = 0; i < n; i++) {
+		if (nums[i] == 0) {
+			nums[++n2] = 2;
+			nums[++n1] = 1;
+			nums[++n0] = 0;
+		}
+		else if (nums[i] == 1) {
+			nums[++n2] = 2;
+			nums[++n1] = 1;
+		}
+		else {
+			nums[++n2] = 2;
+		}
+	}       
+    }
+};
